f2187f61dc16d00cb05e55150a56dd1869c7ab5d
luisantonioj <luisantoniojopia@gmail.com>
2025-09-05 07:43:23 +0800

    remove arrows and indicator in hero

--- src/pages/Home/Hero.jsx
+++ src/pages/Home/Hero.jsx
@@ -38,33 +38,6 @@ export default function Hero({
         ))}
       </div>
 
-      {/* Navigation */}
-      <button
-        onClick={goToPrevSlide}
-        className="absolute left-4 top-1/2 transform -translate-y-1/2 z-10 p-3 bg-black/30 rounded-full hover:bg-black/50"
-      >
-        ◀
-      </button>
-      <button
-        onClick={goToNextSlide}
-        className="absolute right-4 top-1/2 transform -translate-y-1/2 z-10 p-3 bg-black/30 rounded-full hover:bg-black/50"
-      >
-        ▶
-      </button>
-
-      {/* Slide Indicators */}
-      <div className="absolute bottom-6 left-1/2 transform -translate-x-1/2 z-10 flex space-x-2">
-        {heroImages.map((_, index) => (
-          <button
-            key={index}
-            onClick={() => goToSlide(index)}
-            className={`w-3 h-3 rounded-full ${
-              index === currentSlide ? "bg-white" : "bg-white/50"
-            }`}
-          />
-        ))}
-      </div>
-
       {/* Hero Content */}
       <div className="absolute inset-0 flex items-center justify-center">
         <div className="text-center px-4 max-w-4xl">
